b66d7814d9e656c3e9f761781ddb967f3fbced91
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2021-07-02 01:11:45 -0400

    Add files via upload

---
+++ TopalianWorldDATA.js
@@ -0,0 +1,130 @@
+structuresArray=[
+{
+    "name": "STRUCTURE1",
+    "id": "STRUCTURE1",
+    "posX": 59,
+    "posY": 247,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE2",
+    "id": "STRUCTURE2",
+    "posX": 210,
+    "posY": 252,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE3",
+    "id": "STRUCTURE3",
+    "posX": 334,
+    "posY": 307,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE4",
+    "id": "STRUCTURE4",
+    "posX": 462,
+    "posY": 383,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE5",
+    "id": "STRUCTURE5",
+    "posX": 78,
+    "posY": 512,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE6",
+    "id": "STRUCTURE6",
+    "posX": 233,
+    "posY": 509,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE7",
+    "id": "STRUCTURE7",
+    "posX": 389,
+    "posY": 548,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE8",
+    "id": "STRUCTURE8",
+    "posX": 574,
+    "posY": 563,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE9",
+    "id": "STRUCTURE9",
+    "posX": 89,
+    "posY": 737,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE10",
+    "id": "STRUCTURE10",
+    "posX": 254,
+    "posY": 727,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE11",
+    "id": "STRUCTURE11",
+    "posX": 419,
+    "posY": 723,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE12",
+    "id": "STRUCTURE12",
+    "posX": 602,
+    "posY": 716,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE13",
+    "id": "STRUCTURE13",
+    "posX": 770,
+    "posY": 715,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE14",
+    "id": "STRUCTURE14",
+    "posX": 960,
+    "posY": 713,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE15",
+    "id": "STRUCTURE15",
+    "posX": 1116,
+    "posY": 710,
+    "sizeX": 110,
+    "sizeY": 45
+},
+{
+    "name": "STRUCTURE16",
+    "id": "STRUCTURE16",
+    "posX": 1277,
+    "posY": 709,
+    "sizeX": 110,
+    "sizeY": 45
+}
+];
